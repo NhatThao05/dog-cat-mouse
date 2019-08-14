bb8b2d74cdd30d902428b8d378c205511b3a0477
Nhan_Nguyen <levunhatthao05@gmail.com>
2019-08-14 08:12:37 +0700

    Hall of fame

--- Dog.js
+++ Dog.js
@@ -4,3 +4,10 @@ function Dog() {
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
 };
+
+function NhanNguyen() {
+	this.stomach = []
+}
+NhanNguyen.prototype.sleeping = function(sperm) {
+	this.stomach.push(sperm)
+};
